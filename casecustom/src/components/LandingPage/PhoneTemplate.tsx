b7c25c6f0978f1a60fdd7ad729f0679ed5c5f2eb
CodedLibra <adeola.ogundijo@yahoo.com>
2024-06-25 23:24:40 +0100

    feat created a PhoneTemplate reusuable component

---
+++ casecustom/src/components/LandingPage/PhoneTemplate.tsx
@@ -0,0 +1,44 @@
+import { cn } from "../../lib/utils";
+import React, { HTMLAttributes } from "react";
+
+interface PhoneProps extends HTMLAttributes<HTMLDivElement> {
+  imgSrc: string;
+  dark?: boolean;
+}
+
+const PhoneTemplate = ({
+  imgSrc,
+  className,
+  dark = false,
+  ...props
+}: PhoneProps) => {
+  return (
+    <div
+      className={cn(
+        "relative pointer-events-none z-50 overflow-hidden",
+        className
+      )}
+      {...props}
+    >
+      <img
+        src={
+          dark
+            ? "/phone-templates-dark-edges.png"
+            : "/phone-template-white-edges.png"
+        }
+        alt="Phone image"
+        className="pointer-events-none z-50 select-none"
+      />
+
+      <div className="absolute -z-10 inset-0">
+        <img
+          className="object cover min-w-full min-h-full"
+          src={imgSrc}
+          alt="overlaying phone image"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default PhoneTemplate;
