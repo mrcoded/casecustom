75224a005ec0e0ec0f9ab3dfdf0f8f38f96909ff
CodedLibra <adeola.ogundijo@yahoo.com>
2024-06-26 22:49:55 +0100

    feat: added the CustomerReviews component to the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import CustomerReviews from "@/components/LandingPage/CustomerReviews";
 import Hero from "@/components/LandingPage/Hero";
 
 export default function HomePage() {
   return (
     <div className="bg-slate-50">
       <Hero />
+      <CustomerReviews />
     </div>
   );
 }
