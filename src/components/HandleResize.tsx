0c99ba3785dfafe96bdec229771b5d1c69475338
CodedLibra <adeola.ogundijo@yahoo.com>
2024-06-27 00:13:11 +0100

    feat: built a HndleResize component

---
+++ src/components/HandleResize.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const HandleResize = () => {
+  return (
+    <div className="w-5 h-5 rounded-full shadow border bg-white border-zinc-200 transition hover:bg-primary" />
+  );
+};
+
+export default HandleResize;
