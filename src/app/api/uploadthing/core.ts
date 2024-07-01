5ead4cb9f357343e6b1ad8bae4452d61cbac8afa
CodedLibra <adeola.ogundijo@yahoo.com>
2024-07-01 23:19:58 +0100

    chore

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,7 +1,7 @@
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { z } from "zod";
 import sharp from "sharp";
-import { db } from "../../../db";
+import { db } from "@/db";
 
 const f = createUploadthing();
 
