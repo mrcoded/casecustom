53341827a1e8b5e597350df1603ac55914a9a013
CodedLibra <adeola.ogundijo@yahoo.com>
2024-07-05 23:37:00 +0100

    feat: added handleCheckout function to persist configId for user session

--- src/app/configure/preview/DesignPreview.tsx
+++ src/app/configure/preview/DesignPreview.tsx
@@ -12,14 +12,20 @@ import Confetti from "react-dom-confetti";
 import { useMutation } from "@tanstack/react-query";
 import { createCheckoutSession } from "@/lib/actions/preview.actions";
 import { useRouter } from "next/navigation";
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
 import { useToast } from "@/components/ui/use-toast";
+import LoginModal from "@/components/modals/LoginModal";
 
 const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
   const router = useRouter();
   const { toast } = useToast();
+  const { id } = configuration;
+  const { user } = useKindeBrowserClient();
+
+  const [isLoginModalOpen, setIsLoginModalOpen] = useState<boolean>(false);
 
   //configure confetti
-  const [showConfetti, setShowConfetti] = useState(false);
+  const [showConfetti, setShowConfetti] = useState<boolean>(false);
   useEffect(() => setShowConfetti(true));
 
   const { color, model, finish, material } = configuration;
@@ -59,6 +65,17 @@ const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
     },
   });
 
+  const handleCheckout = () => {
+    if (user) {
+      // create payment session
+      createPaymentSession({ configId: id });
+    } else {
+      // need to log in
+      localStorage.setItem("configurationId", id);
+      setIsLoginModalOpen(true);
+    }
+  };
+
   return (
     <>
       <div
@@ -70,6 +87,8 @@ const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
           config={{ elementCount: 200, spread: 90 }}
         />
       </div>
+
+      <LoginModal isOpen={isLoginModalOpen} setIsOpen={setIsLoginModalOpen} />
 
       <div className="mt-20 grid grid-cols-1 text-sm sm:grid-cols-12 sm:grid-rows-1 sm:gap-x-8 lg:gap-x-12">
         <div className="sm:col-span-4 md:col-span-3 md:row-span-2 md:row-end-2">
@@ -154,9 +173,7 @@ const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
                 // isLoading={true}
                 // loadingText="Loading"
                 className="px-4 sm:px-6 lg:px-8"
-                onClick={() =>
-                  createCheckoutSession({ configId: configuration.id })
-                }
+                onClick={() => handleCheckout()}
               >
                 Check out <ArrowRight className="h-4 w-4 ml-1.5 inline  " />
               </Button>
