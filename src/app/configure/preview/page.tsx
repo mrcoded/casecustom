a87c5963f495e90a12980abf85872a842eb4afec
CodedLibra <adeola.ogundijo@yahoo.com>
2024-07-05 23:37:37 +0100

    feat: built a configure/preview page

---
+++ src/app/configure/preview/page.tsx
@@ -0,0 +1,29 @@
+import { notFound } from "next/navigation";
+import React from "react";
+import { db } from "../../../db";
+import DesignPreview from "./DesignPreview";
+
+interface PageProps {
+  searchParams: {
+    [key: string]: string | string[] | undefined;
+  };
+}
+const Page = async ({ searchParams }: PageProps) => {
+  const { id } = searchParams;
+
+  if (!id || typeof id !== "string") {
+    return notFound();
+  }
+
+  const configuraton = await db.configuration.findUnique({
+    where: { id },
+  });
+
+  if (!configuraton) {
+    return notFound();
+  }
+
+  return <DesignPreview configuration={configuraton} />;
+};
+
+export default Page;
