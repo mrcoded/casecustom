97dba0c2d75aabcf62567ba018990e7c72f3b02f
CodedLibra <adeola.ogundijo@yahoo.com>
2024-07-17 19:50:48 +0100

    feat: added more order details to page

--- src/components/PaymentCompleted.tsx
+++ src/components/PaymentCompleted.tsx
@@ -5,6 +5,8 @@ import { useQuery } from "@tanstack/react-query";
 import { Loader2 } from "lucide-react";
 import { useSearchParams } from "next/navigation";
 import React from "react";
+import PhonePreview from "./PhonePreview";
+import { formatPrice } from "@/lib/utils";
 
 const PaymentCompleted = () => {
   const searchParams = useSearchParams();
@@ -30,17 +32,23 @@ const PaymentCompleted = () => {
   }
 
   if (data === false) {
-    <div className="w-full mt-24 flex justify-center">
-      <div className="flex flex-col items-center gap-2">
-        <Loader2 className="h-8 w-8 animate-spin text-zinc-500" />
-        <h3 className="font-semibold text-xl">Verifying your payment......</h3>
-        <p>This might take moment.</p>
+    return (
+      <div className="w-full mt-24 flex justify-center">
+        <div className="flex flex-col items-center gap-2">
+          <Loader2 className="h-8 w-8 animate-spin text-zinc-500" />
+          <h3 className="font-semibold text-xl">
+            Verifying your payment......
+          </h3>
+          <p>This might take moment.</p>
+        </div>
       </div>
-    </div>;
+    );
   }
 
+  console.log(data);
   const { configuration, billingAddress, shippingAddress, amount } = data;
-  // const { color } = configuration;
+  console.log(configuration);
+  const { color } = configuration;
 
   return (
     <div className="bg-white">
@@ -57,6 +65,83 @@ const PaymentCompleted = () => {
           <div className="mt-12 text-sm font-medium">
             <p className="text-zinc-900">Order number</p>
             <p className="mt-2 text-zinc-500">{orderId}</p>
+          </div>
+        </div>
+
+        <div className="mt-10 border-t border-zinc-200">
+          <div className="mt-10 flex flex-auto flex-col">
+            <h4 className="font-semibold text-zinc-900">
+              You made a great choice!
+            </h4>
+            <p className="mt-2 text-sm text-zinc-600">
+              We at CaseCustom beleives in quality phone case that looks good
+              and lasts longer, therefore we offer a 3-years print gurantee.
+            </p>
+          </div>
+        </div>
+
+        <div className="flex space-x-6 overflow-hidden mt-4 rounded-xl bg-gray-900/5 ring-1 ring-inset ring-gray-900/10 lg:rounded-2xl">
+          <PhonePreview
+            croppedImageUrl={configuration.croppedImageUrl!}
+            color={color!}
+          />
+        </div>
+
+        <div>
+          <div className="grid grid-cols-2 gap-x-6 py-10 text-sm">
+            <div>
+              <p className="font-medium text-grat-900">Shipping address</p>
+              <div className="mt-2 text-zinc-700">
+                <address className="not-italic">
+                  <span className="block">{shippingAddress?.name}</span>
+                  <span className="block">{shippingAddress?.street}</span>
+                  <span className="block">
+                    {shippingAddress?.postalCode} {shippingAddress?.city}
+                  </span>
+                </address>
+              </div>
+            </div>
+            <div>
+              <p className="font-medium text-grat-900">Billing address</p>
+              <div className="mt-2 text-zinc-700">
+                <address className="not-italic">
+                  <span className="block">{billingAddress?.name}</span>
+                  <span className="block">{billingAddress?.street}</span>
+                  <span className="block">
+                    {billingAddress?.postalCode} {billingAddress?.city}
+                  </span>
+                </address>
+              </div>
+            </div>
+          </div>
+
+          <div className="grid grid-cols-2 gap-x-6 border-t border-zinc-200 py-10 text-sm">
+            <div>
+              <p className="font-medium text-zinc-900">Payment status</p>
+              <p className="mt-2 text-zinc-700">Paid</p>
+            </div>
+
+            <div>
+              <p className="font-medium text-zinc-900">Shipping status</p>
+              <p className="mt-2 text-zinc-700">
+                DHL shipping takes up to 3 working days
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="space-y-6 border-t border-zinc-200 pt-10 text-sm">
+          <div className="flex justify-between">
+            <p className="font-medium text-zinc-900">Subtotal</p>
+            <p className="font-medium text-zinc-700">{formatPrice(amount)}</p>
+          </div>
+          <div className="flex justify-between">
+            <p className="font-medium text-zinc-900">Shipping</p>
+            <p className="font-medium text-zinc-700">{formatPrice(0)}</p>
+          </div>
+          <div className="flex justify-between">
+            <p className="font-medium text-zinc-900">Total</p>
+            <p className="font-medium text-zinc-700">{formatPrice(amount)}</p>
           </div>
         </div>
       </div>
