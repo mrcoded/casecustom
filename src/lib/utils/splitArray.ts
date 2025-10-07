1a481e27373366f4279bf2443afef2cc39140bb6
CodedLibra <adeola.ogundijo@yahoo.com>
2025-10-07 23:28:00 +0100

    created splitArray and base64ToBlob utils functions

---
+++ src/lib/utils/splitArray.ts
@@ -0,0 +1,12 @@
+export function splitArray<T>(array: Array<T>, numParts: number) {
+  const result: Array<Array<T>> = [];
+
+  for (let i = 0; i < array.length; i++) {
+    const index = i % numParts;
+    if (!result[index]) {
+      result[index] = [];
+    }
+    result[index].push(array[i]);
+  }
+  return result;
+}
