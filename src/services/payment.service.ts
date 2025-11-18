33ba6a3280f62e0640271272623834a92b89344b
CodedLibra <adeola.ogundijo@yahoo.com>
2025-11-18 20:44:53 +0100

    feat: created payment service

---
+++ src/services/payment.service.ts
@@ -0,0 +1,32 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useMutation } from "@tanstack/react-query";
+
+import { useToast } from "@/components/ui/use-toast";
+import { createCheckoutSession } from "@/lib/actions/preview.actions";
+
+export function paymentService({ configId }: { configId: string }) {
+  const { toast } = useToast();
+  const router = useRouter();
+
+  return useMutation({
+    mutationKey: ["create-checkout-session"],
+    mutationFn: () => createCheckoutSession({ configId }),
+    onSuccess: ({ url }: { url: string | null }) => {
+      if (url) {
+        router.push(url);
+      } else {
+        throw new Error("Unable to retrieve payment URL.");
+      }
+    },
+    onError: (error) => {
+      console.log(error);
+      toast({
+        title: "Something went wrong",
+        description: "There was an error! Please try again.",
+        variant: "destructive",
+      });
+    },
+  });
+}
