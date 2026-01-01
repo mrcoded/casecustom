4dc6f162223e4f31d321563f6f14ca5f7aafddde
CodedLibra <adeola.ogundijo@yahoo.com>
2026-01-01 23:34:25 +0100

    feat: added logoutService and loginService function

--- src/services/login.service.ts
+++ src/services/login.service.ts
@@ -1,9 +1,18 @@
 "use client";
 
-import { LoginProps } from "@/types/auth";
-import { signIn, SignInResponse } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import {
+  signIn,
+  SignInResponse,
+  signOut,
+  SignOutResponse,
+} from "next-auth/react";
+import { useMutation } from "@tanstack/react-query";
 
-export const LoginServiceFn = async (
+import { LoginProps } from "@/types/auth";
+import { useToast } from "@/components/ui/use-toast";
+
+const loginServiceFn = async (
   data: LoginProps
 ): Promise<SignInResponse | undefined> => {
   return await signIn("credentials", {
@@ -12,3 +21,45 @@ export const LoginServiceFn = async (
     redirect: false,
   });
 };
+
+// Logout function handler
+export const logoutServiceFn = async (): Promise<
+  SignOutResponse | undefined
+> => {
+  return await signOut({ callbackUrl: "/", redirect: true });
+};
+
+export function loginService() {
+  const router = useRouter();
+  const { toast } = useToast();
+
+  //login mutation handler
+  return useMutation({
+    mutationFn: loginServiceFn,
+    onSuccess: (data) => {
+      if (data?.ok) {
+        toast({
+          title: "Success",
+          description: "Login Successful! Redirecting...",
+          variant: "default",
+        });
+
+        router.push("/configure/upload");
+      } else {
+        console.log(data);
+        toast({
+          title: "Something went wrong",
+          description: `${data?.error}`,
+          variant: "destructive",
+        });
+      }
+    },
+    onError: (error) => {
+      toast({
+        title: "Something went wrongs",
+        description: `${error.message}`,
+        variant: "destructive",
+      });
+    },
+  });
+}
