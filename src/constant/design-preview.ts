35b4be78132f5f3681afb24398fe2cf2d37382bc
CodedLibra <adeola.ogundijo@yahoo.com>
2026-01-02 15:24:00 +0100

    created a constant utils for design-preview

---
+++ src/constant/design-preview.ts
@@ -0,0 +1,16 @@
+import { BASE_PRICE, PRODUCT_PRICES } from "./product";
+
+export const DESIGN_PRICE = [
+  {
+    label: "Base price",
+    price: BASE_PRICE,
+  },
+  {
+    label: "Textured finish",
+    price: PRODUCT_PRICES.finish.textured,
+  },
+  {
+    label: "Soft polycarbonate material",
+    price: PRODUCT_PRICES.material.polycarbonate,
+  },
+];
