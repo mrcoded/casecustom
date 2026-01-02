c6557dd5f2dc4f5c860dc49f4ed5998314d5c916
CodedLibra <adeola.ogundijo@yahoo.com>
2026-01-02 15:20:49 +0100

    feat: wrapped file in suspense and AuthProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,17 @@
+import { Suspense } from "react";
 import type { Metadata } from "next";
+
 import { Recursive } from "next/font/google";
 import "./globals.css";
+
+import Loading from "./loading";
 import Navbar from "@/components/shared/Navbar";
 import Footer from "@/components/shared/Footer";
 import { Toaster } from "@/components/ui/toaster";
-import Providers from "@/lib/Providers";
+import QueryProvider from "@/providers/QueryProviders";
+
 import { constructMetadata } from "@/lib/utils";
+import AuthProvider from "@/providers/AuthProvider";
 
 const recursive = Recursive({ subsets: ["latin"] });
 
@@ -19,14 +25,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={recursive.className}>
-        <Navbar />
+        <Suspense fallback={<Loading />}>
+          <AuthProvider>
+            <Navbar />
 
-        <main className="flex grainy-light flex-col min-h-[calc(100vh-3.5rem-1px)]">
-          <div className="flex flex-1 flex-col h-full">
-            <Providers>{children}</Providers>
-          </div>
-          <Footer />
-        </main>
+            <main className="flex grainy-light flex-col min-h-[calc(100vh-3.5rem-1px)]">
+              <div className="flex flex-1 flex-col h-full">
+                <QueryProvider>{children}</QueryProvider>
+              </div>
+              <Footer />
+            </main>
+          </AuthProvider>
+        </Suspense>
 
         <Toaster />
       </body>
