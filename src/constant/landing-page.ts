200c4a3106bf98a361d97ad68492a8ed061b2a88
CodedLibra <adeola.ogundijo@yahoo.com>
2026-01-02 15:12:45 +0100

    feat: created a constant file for landing-page

---
+++ src/constant/landing-page.ts
@@ -0,0 +1,51 @@
+export const HERO_IMAGE = [
+  { index: 0, src: "/users/user-1.png" },
+  { index: 1, src: "/users/user-2.png" },
+  { index: 2, src: "/users/user-3.png" },
+  { index: 3, src: "/users/user-4.jpg" },
+  { index: 4, src: "/users/user-5.jpg", className: "object-cover" },
+];
+
+export const GETTING_STARTED = [
+  { text: "High-quality silicon material" },
+  { text: "Scratch and fingerprint resistant coating" },
+  { text: "Wireless charging compatible" },
+  { text: "5 year print warranty" },
+];
+
+export const POSSIBLE_ANINMATION_DELAYS = [
+  "0s",
+  "0.1s",
+  "0.2s",
+  "0.3s",
+  "0.4s",
+  "0.5s",
+];
+
+export const PHONES = [
+  "/testimonials/1.jpg",
+  "/testimonials/2.jpg",
+  "/testimonials/3.jpg",
+  "/testimonials/4.jpg",
+  "/testimonials/5.jpg",
+  "/testimonials/6.jpg",
+];
+
+export const CUSTOMER_REVIEWS = [
+  {
+    name: "Jonathan",
+    img: "/users/user-1.png",
+    textStart: `The case feels durable and i even got a compliment on the design. I have had the case for two and a half months now and`,
+    textMiddle: "the image is super clear",
+    textEnd:
+      ", on the case i had before, the image started fading into yellow-ish color after a couple weeks. Love it.",
+  },
+  {
+    name: "Josh",
+    img: "/users/user-4.jpg",
+    textStart: `I usually keep my phone together with my keys in my pocket and that led to some pretty heavy scratchmarks on all of my last
+                phone cases. This one, besides a barely noticeable scratch on the corner,`,
+    textMiddle: "looks brand new after about half a year",
+    textEnd: ". I dig it!",
+  },
+];
