0fafbb14081e4457d648e7be70fd817e03bc3461
CodedLibra <adeola.ogundijo@yahoo.com>
2026-01-02 17:13:46 +0100

    removed resend import

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -3,10 +3,7 @@ import { stripe } from "@/lib/stripe";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
-import { Resend } from "resend";
 import OrderRecievedEmail from "@/components/emails/OrderRecievedEmail";
-
-const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req: Request) {
   try {
